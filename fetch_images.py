e6eadea730f071d26671d9783e60b6458e671cd8
Jakub Lechowski <b.kuba.13@gmail.com>
2024-11-21 00:56:18 +0100

    fix bbox coords error

--- fetch_images.py
+++ fetch_images.py
@@ -20,25 +20,28 @@ import numpy as np
 import shutil
 import time
 
+
 class FetchImages:
     def __init__(
-         self,
-         data: pd.DataFrame,
-         config: object,
-         evalscript: str,
-         threads: int = 5,
-         size_km=50,
-         resolution: int = 100,
-         time_interval_size=5,
-         verbose=False,
-         mosaicking_order: MosaickingOrder = MosaickingOrder.LEAST_CC,
-         save_dir: str = "./satelite_data"
+        self,
+        data: pd.DataFrame,
+        config: object,
+        evalscript: str,
+        threads: int = 5,
+        size_km=50,
+        resolution: int = 100,
+        time_interval_size=5,
+        verbose=False,
+        mosaicking_order: MosaickingOrder = MosaickingOrder.LEAST_CC,
+        save_dir: str = "./satelite_data",
     ):
         self.data = data
         self.config = config
         self.evalscript = evalscript
         self.threads = threads
-        self.size_km = size_km // 2 # due to the fact that, original value represent half of box edge len
+        self.size_km = (
+            size_km // 2
+        )  # due to the fact that, original value represent half of box edge len
         self.resolution = resolution
         self.time_interval_size = time_interval_size
         self.verbose = verbose
@@ -59,37 +62,43 @@ class FetchImages:
                 size=size,
             )
             list_of_requests.append(request.download_list[0])
-        print("Loading...") 
+        print("Loading...")
         data = SentinelHubDownloadClient(config=self.config).download(
-            list_of_requests,
-            max_threads=self.threads
+            list_of_requests, max_threads=self.threads
         )
         print(f"Done! Results save in {self.save_dir}")
         return self
 
     def _define_time_interval(self, row):
-        return (row["Date_standard"] - timedelta(days=self.time_interval_size), row["Date_standard"] + timedelta(days=self.time_interval_size))
+        return (
+            row["Date_standard"] - timedelta(days=self.time_interval_size),
+            row["Date_standard"] + timedelta(days=self.time_interval_size),
+        )
 
-    def _define_bbox(self, latitude: float, longitude: float) -> Tuple[BBox, Tuple[int]]:
-        km_per_degree = 111 # 1 degree = 111 km approximately
-        
+    def _define_bbox(
+        self, latitude: float, longitude: float
+    ) -> Tuple[BBox, Tuple[int]]:
+        km_per_degree = 111  # 1 degree = 111 km approximately
+
         lat_offset = self.size_km / km_per_degree
         lon_offset = lat_offset / math.cos(math.radians(latitude))
-        
+
         lat_min = latitude - lat_offset
         lat_max = latitude + lat_offset
         lon_min = longitude - lon_offset
         lon_max = longitude + lon_offset
-        
-        bbox_WGS84 = [lat_min, lon_min, lat_max, lon_max]
+
+        bbox_WGS84 = [lon_min, lat_min, lon_max, lat_max]
         bbox = BBox(bbox=bbox_WGS84, crs=CRS.WGS84)
         size = bbox_to_dimensions(bbox, resolution=self.resolution)
-    
+
         if self.verbose:
-            print("To see actual bbox go to this page http://bboxfinder.com/#0.000000,0.000000,0.000000,0.000000 and paste cords")
+            print(
+                "To see actual bbox go to this page http://bboxfinder.com/#0.000000,0.000000,0.000000,0.000000 and paste cords"
+            )
             print(f"({lon_min}, {lat_min}, {lon_max}, {lat_max})")
         return bbox, size
-        
+
     def _get_request(
         self,
         time_interval: Tuple[datetime],
@@ -104,7 +113,7 @@ class FetchImages:
                         "s2l1c", service_url=self.config.sh_base_url
                     ),
                     time_interval=time_interval,
-                    mosaicking_order=self.mosaicking_order
+                    mosaicking_order=self.mosaicking_order,
                 )
             ],
             responses=[SentinelHubRequest.output_response("default", MimeType.PNG)],
@@ -117,37 +126,48 @@ class FetchImages:
 
     def rename_request_png_based_on_json(self):
         for subdir, dirs, files in os.walk(self.save_dir):
-            if 'request.json' in files and 'response.png' in files:  # Check both files exist
-                json_file_path = os.path.join(subdir, 'request.json')
-                png_file_path = os.path.join(subdir, 'response.png')
-                
+            if (
+                "request.json" in files and "response.png" in files
+            ):  # Check both files exist
+                json_file_path = os.path.join(subdir, "request.json")
+                png_file_path = os.path.join(subdir, "response.png")
+
                 print(f"Processing JSON file: {json_file_path}")
                 try:
                     # Open and read the JSON file
-                    with open(json_file_path, 'r') as json_file:
+                    with open(json_file_path, "r") as json_file:
                         data = json.load(json_file)
                         # Extract the relevant value (e.g., bbox from JSON)
-                        value = tuple(data["request"]["payload"]["input"]["bounds"]["bbox"])
-                        
+                        value = tuple(
+                            data["request"]["payload"]["input"]["bounds"]["bbox"]
+                        )
+
                         if value and value in self.mapper:
                             # Get the new file name from the mapping
                             new_file_name = str(self.mapper[value])
-                            new_png_file_path = os.path.join(subdir, f"{new_file_name}.png")
-                            
+                            new_png_file_path = os.path.join(
+                                subdir, f"{new_file_name}.png"
+                            )
+
                             # Retry logic to rename the file if it's locked
                             retries = 3
                             for attempt in range(retries):
                                 try:
-                                    shutil.move(png_file_path, new_png_file_path)  # Rename the PNG file
-                                    print(f'Renamed {png_file_path} to {new_png_file_path}')
+                                    shutil.move(
+                                        png_file_path, new_png_file_path
+                                    )  # Rename the PNG file
+                                    print(
+                                        f"Renamed {png_file_path} to {new_png_file_path}"
+                                    )
                                     break  # Exit loop if successful
                                 except PermissionError as e:
                                     print(f"Attempt {attempt + 1} failed: {e}")
                                     if attempt < retries - 1:
                                         time.sleep(1)  # Wait before retrying
                                     else:
-                                        print(f"Failed to rename {png_file_path} after {retries} attempts.")
+                                        print(
+                                            f"Failed to rename {png_file_path} after {retries} attempts."
+                                        )
                                         raise  # Re-raise the exception if all attempts fail
                 except Exception as e:
                     print(f"Error processing {json_file_path}: {e}")
-        
